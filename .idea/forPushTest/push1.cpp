94bf933e0f7ca50fad446416c71d609d24cdfdb1
gaoyang <gaoy_1@outlook.com>
2019-08-20 16:35:31 +0800

    gy add for PushTset/push1.cpp/.h

---
+++ .idea/forPushTest/push1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gaoyang on 19-8-20.
+//
+
+#include "push1.h"
---
+++ .idea/forPushTest/push1.h
@@ -0,0 +1,16 @@
+//
+// Created by gaoyang on 19-8-20.
+//
+
+#ifndef NEWTSEARCH_PUSH1_H
+#define NEWTSEARCH_PUSH1_H
+
+
+
+class push1 {
+
+};
+
+
+
+#endif //NEWTSEARCH_PUSH1_H
